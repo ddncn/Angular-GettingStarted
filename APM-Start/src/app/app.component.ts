c104ca80ab413038232b8cd323d2c7d7b6c2e1de
David Duncan <dm.dncn@live.com>
2017-10-16 19:22:56 -0500

    Syncing with latest changes from tutorial. Ready to start section: Building Nested Components.

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-    <div><h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
-    </div>
+  <div><h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+  </div>
   `
 })
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management';
+  pageTitle: string = "Acme Product Management";
 }
